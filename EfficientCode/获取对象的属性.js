d6113e17555de46f56b4b890b6c2d303beb59752
yxy <a1098011427@qq.com>
2021-04-22 09:02:51 +0800

    .

--- EfficientCode/获取对象的属性.js
+++ EfficientCode/获取对象的属性.js
@@ -2,30 +2,33 @@ let obj = {
   play: {
     p1: {
       atk: '10',
-      def: '5'
+      def: '5',
     },
     p2: {
       atk: '无限',
-      def: '为零'
-    }
+      def: '为零',
+    },
   },
-  game: [{
-    name: '恐龙快打',
-    member: ['p1', 'p2']
-  }, {
-    name: '忍者神龟',
-    member: ['p1']
-  }]
+  game: [
+    {
+      name: '恐龙快打',
+      member: ['p1', 'p2'],
+    },
+    {
+      name: '忍者神龟',
+      member: ['p1'],
+    },
+  ],
 }
 
 console.log(getObjProperty(obj, 'play.p1.atk', 'play.p2.def', 'game[0].member'))
 
 function getObjProperty(obj, ...selector) {
-  let res = selector.map(x => {
+  let res = selector.map((x) => {
     let s = x.replace(/\[(\w+)\]/g, '.$1').split('.')
     return s.reduce((pre, cur) => {
       return pre && pre[cur]
-    }, obj) 
+    }, obj)
   })
   return res
 }
--- GloryofKing/game/player.js
+++ GloryofKing/game/player.js
@@ -1,17 +1,17 @@
-import Luban from './heroes/luban.js';
-import Yase from './heroes/yase.js';
- 
-export default class  Player{
-    constructor(name){
-        this.name = name;
-        this.heroes = [new Yase(), new Luban()];
-        this.selectHero = ''
-        this.selectSkin = ''
-    }
-    heroSelected(hero) {
-        this.selectHero = hero
-    }
-    skinSelected(skin) {
-        this.selectSkin = skin
-    }
+import Luban from './heroes/luban.js'
+import Yase from './heroes/yase.js'
+
+export default class Player {
+  constructor(name) {
+    this.name = name
+    this.heroes = [new Yase(), new Luban()]
+    this.selectHero = ''
+    this.selectSkin = ''
+  }
+  heroSelected(hero) {
+    this.selectHero = hero
+  }
+  skinSelected(skin) {
+    this.selectSkin = skin
+  }
 }
