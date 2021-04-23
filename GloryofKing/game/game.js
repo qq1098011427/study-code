8906805773f5dda1d04c5858ce02415a6070fb0d
yxy <a1098011427@qq.com>
2021-04-23 10:17:05 +0800

    game类继承观察者模式

--- GloryofKing/game/game.js
+++ GloryofKing/game/game.js
@@ -1,9 +1,17 @@
 import Player from './player.js';
-export default class Game{
+import Event from './event.js'
+export default class Game extends Event{
     constructor(){
+        super()
         this.player = null;
     }
     login(name){
+        this.addEvent('init', (hero) => {
+            console.log(hero + '登录初始化')
+        })
         this.player = new Player(name);
+        this.player.heroes.forEach(hero => {
+            this.trigger('init', hero.name)
+        })
     }
 }
